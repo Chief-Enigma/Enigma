ec74b831c6867996e9c3cf0a125f36e563a5ecd3
Julian <julian.bambauer@bluewin.ch>
2021-07-08 22:02:14 +0200

    Seperated funktion in class

---
+++ Enigma C#/Firststeps2/Encryption.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Diagnostics;
+
+namespace EncryptionSoftware
+{
+    public class Encryption
+    {
+
+        static char[] characters = { 'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', ' ', '.', ',', '?', '!', '-', '1', '2', '3', '4', '5', '6', '7', '8', '9', '0', '\'' };
+        static int[] key = { 1, 17, 63, 8, 2, 19, 22, 1, 45, 2, 4, 6, 2, 0, 17, 5, 1, 48 };
+
+        public static string encrypt_message(string Message_In)
+        {
+            List<int> Arr1 = new List<int>();
+            List<char> Arr2 = new List<char>();
+            string Message_Encrypted = "";
+            string encrypted_message_out = "";
+
+            char[] Message_In_arr = Message_In.ToCharArray();
+
+
+            for (int x = 0; x < Message_In.Length; x++)
+            {
+                for (int y = 0; y < characters.Length; y++)
+                {
+                    char lookingfor = characters[y];
+                    if (Message_In_arr[x] == lookingfor)
+                    {
+                        Arr1.Add(y);
+                    }
+                }
+            }
+            //numbers in arr
+            for (int y = 0; y < Message_In.Length; y++)
+            {
+                Arr1[y] = (Arr1[y] + key[y % key.Length]);
+            }
+            //build encrypted message
+            for (int y = 0; y < Arr1.Count; y++)
+            {
+                //Arr2.append(characters[Arr1[y] % characters.Length]);
+                Arr2.Add(characters[Arr1[y] % characters.Length]);
+            }
+
+            for (int y = 0; y < Arr2.Count; y++)
+            {
+                Message_Encrypted += Arr2[y];
+            }
+
+            return encrypted_message_out = Message_Encrypted;
+            //Console.WriteLine("Message Encrypted: " + Message_Encrypted);
+            //Console.Write("Message Encrypted: ");
+
+            //for (int y = 0; y < Arr2.Count; y++)
+            //{
+            //    for (int z = 0; z < characters.Length; z++)
+            //    {
+            //        Console.Write(characters[z]);
+            //        if (characters[z] == Arr2[y])
+            //        {
+            //            break;
+            //        }
+            //        System.Threading.Thread.Sleep((2000 / (Message_Encrypted.Length)) / characters.Length);
+            //        Console.Write("\b"); // Move the cursor one unit to the left
+            //                             //Console.Write("\x1B[1P"); // Delete the character
+
+            //    }
+
+            //}
+            //Console.WriteLine();
+
+        }
+
+
+
+        public static string decrypt_message(string Message_In)
+        {
+            List<int> Arr1 = new List<int>();
+            List<char> Arr2 = new List<char>();
+            string Message_Encrypted = "";
+            string decrypted_message_out = "";
+
+            char[] Message_In_arr = Message_In.ToCharArray();
+
+            for (int x = 0; x < Message_In.Length; x++)
+            {
+                for (int y = 0; y < characters.Length; y++)
+                {
+                    char lookingfor = characters[y];
+                    if (Message_In_arr[x] == lookingfor)
+                    {
+                        Arr1.Add(y);
+                    }
+                }
+            }
+            //numbers in arr
+            for (int y = 0; y < Message_In.Length; y++)
+            {
+                Arr1[y] = (Arr1[y] - key[y % key.Length]);
+            }
+            //build encrypted message
+            for (int y = 0; y < Arr1.Count; y++)
+            {
+                //Arr2.append(characters[Arr1[y] % characters.Length]);
+                int index = Arr1[y] % characters.Length;
+                if (index < 0)
+                {
+                    index = characters.Length + index;
+                }
+                Arr2.Add(characters[index]);
+
+            }
+
+            for (int y = 0; y < Arr2.Count; y++)
+            {
+                Message_Encrypted += Arr2[y];
+            }
+
+            return decrypted_message_out = Message_Encrypted;
+
+            ////Console.WriteLine("Message Decrypted: " + Message_Encrypted);
+            //Console.Write("Message Decrypted: ");
+
+            //for (int y = 0; y < Arr2.Count; y++)
+            //{
+            //    for (int z = 0; z < characters.Length; z++)
+            //    {
+            //        Console.Write(characters[z]);
+            //        if (characters[z] == Arr2[y])
+            //        {
+            //            break;
+            //        }
+            //        System.Threading.Thread.Sleep((2000 / (Message_Encrypted.Length)) / characters.Length);
+            //        Console.Write("\b"); // Move the cursor one unit to the left
+            //                             //Console.Write("\x1B[1P"); // Delete the character
+
+            //    }
+
+            //}
+            //Console.WriteLine();
+        }
+    }
+}
--- Enigma C#/Firststeps2/Program.cs
+++ Enigma C#/Firststeps2/Program.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Diagnostics;
+using EncryptionSoftware;
 
 class Program
 {
@@ -9,7 +10,6 @@ class Program
 
 
 
-    //ddd
     static void Main()
     {
 
@@ -25,13 +25,15 @@ class Program
             {
                 Console.WriteLine("Rules: only use lowercase  letters, NO capital letter!");
                 Console.Write("Input Message: ");
-                encrypt_message(Console.ReadLine());
+                Console.WriteLine("Encrypted message: " + Encryption.encrypt_message(Console.ReadLine()));
             }
             else if (choice == "2")
             {
                 Console.WriteLine("Rules: only use lowercase  letters, NO capital letter!");
                 Console.Write("Input encrypted Message: ");
-                decrypt_message(Console.ReadLine());
+
+                Console.WriteLine("Decrypted message: " + Encryption.decrypt_message(Console.ReadLine()));
+                
             }
             else
             {
@@ -44,146 +46,20 @@ class Program
 
     }
 
-    static void encrypt_message(string Message_In)
-    {
-        List<int> Arr1 = new List<int>();
-        List<char> Arr2 = new List<char>();
-        string Message_Encrypted = "";
-
-        char[] Message_In_arr = Message_In.ToCharArray();
-        
-
-        for (int x = 0; x < Message_In.Length; x++)
-        {
-            for (int y = 0; y < characters.Length; y++)
-            {
-                char lookingfor = characters[y];
-                if (Message_In_arr[x] == lookingfor)
-                {
-                    Arr1.Add(y);
-                }
-            }
-        }
-        //numbers in arr
-        for (int y = 0; y < Message_In.Length; y++)
-        {
-            Arr1[y] = (Arr1[y] + key[y % key.Length]);
-        }
-        //build encrypted message
-        for (int y = 0; y < Arr1.Count; y++)
-        {
-            //Arr2.append(characters[Arr1[y] % characters.Length]);
-            Arr2.Add(characters[Arr1[y] % characters.Length]);
-        }
-
-        for (int y = 0; y < Arr2.Count; y++)
-        {
-            Message_Encrypted += Arr2[y];
-        }
-
-        //Console.WriteLine("Message Encrypted: " + Message_Encrypted);
-        Console.Write("Message Encrypted: ");
-
-        for (int y = 0; y < Arr2.Count; y++)
-        {
-            for (int z = 0; z < characters.Length; z++)
-            {
-                Console.Write(characters[z]);
-                if (characters[z] == Arr2[y])
-                {
-                    break;
-                }
-                System.Threading.Thread.Sleep((2000 / (Message_Encrypted.Length)) / characters.Length);
-                Console.Write("\b"); // Move the cursor one unit to the left
-                //Console.Write("\x1B[1P"); // Delete the character
-                
-            }
-        
-        }
-        Console.WriteLine();
-
-    }
-
-
-
-    static void decrypt_message(string Message_In)
-    {
-        List<int> Arr1 = new List<int>();
-        List<char> Arr2 = new List<char>();
-        string Message_Encrypted = "";
-
-        char[] Message_In_arr = Message_In.ToCharArray();
-
-        for (int x = 0; x < Message_In.Length; x++)
-        {
-            for (int y = 0; y < characters.Length; y++)
-            {
-                char lookingfor = characters[y];
-                if (Message_In_arr[x] == lookingfor)
-                {
-                    Arr1.Add(y);
-                }
-            }
-        }
-        //numbers in arr
-        for (int y = 0; y < Message_In.Length; y++)
-        {
-            Arr1[y] = (Arr1[y] - key[y % key.Length]);
-        }
-        //build encrypted message
-        for (int y = 0; y < Arr1.Count; y++)
-        {
-            //Arr2.append(characters[Arr1[y] % characters.Length]);
-            int index = Arr1[y] % characters.Length;
-            if (index < 0)
-            {
-                index = characters.Length + index;
-            }
-            Arr2.Add(characters[index]);
-
-        }
-
-        for (int y = 0; y < Arr2.Count; y++)
-        {
-            Message_Encrypted += Arr2[y];
-        }
-
-        //Console.WriteLine("Message Decrypted: " + Message_Encrypted);
-        Console.Write("Message Decrypted: ");
-
-        for (int y = 0; y < Arr2.Count; y++)
-        {
-            for (int z = 0; z < characters.Length; z++)
-            {
-                Console.Write(characters[z]);
-                if (characters[z] == Arr2[y])
-                {
-                    break;
-                }
-                System.Threading.Thread.Sleep((2000 / (Message_Encrypted.Length))/characters.Length);
-                Console.Write("\b"); // Move the cursor one unit to the left
-                //Console.Write("\x1B[1P"); // Delete the character
-
-            }
-
-        }
-        Console.WriteLine();
-    }
-
     static void print_label()
     {
 
-        Console.WriteLine("        .-\"\"-.                                           _   _                            __ _");
-        Console.WriteLine("       / .--. \\          ___ _ __   ___ _ __ _   _ _ __ | |_(_) ___  _ __      ___  ___  / _| |___      ____ _ _ __ ___ ");
-        Console.WriteLine("      / /    \\ \\        / _ \\ '_ \\ / __| '__| | | | '_ \\| __| |/ _ \\| '_ \\    / __|/ _ \\| |_| __\\ \\ /\\ / / _` | '__/ _ \\");
-        Console.WriteLine("      | |    | |       |  __/ | | | (__| |  | |_| | |_) | |_| | (_) | | | |   \\__ \\ (_) |  _| |_ \\ V  V / (_| | | |  __/");
-        Console.WriteLine("      | |.-\"\"-.|        \\___|_| |_|\\___|_|   \\__, | .__/ \\__|_|\\___/|_| |_|   |___/\\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___|");
-        Console.WriteLine("     ///`.::::.`\\                            |___/|_|");
+        Console.WriteLine("        .-\"\"-.                                       _   _                            __ _");
+        Console.WriteLine("       / .--. \\      ___ _ __   ___ _ __ _   _ _ __ | |_(_) ___  _ __      ___  ___  / _| |___      ____ _ _ __ ___ ");
+        Console.WriteLine("      / /    \\ \\    / _ \\ '_ \\ / __| '__| | | | '_ \\| __| |/ _ \\| '_ \\    / __|/ _ \\| |_| __\\ \\ /\\ / / _` | '__/ _ \\");
+        Console.WriteLine("      | |    | |   |  __/ | | | (__| |  | |_| | |_) | |_| | (_) | | | |   \\__ \\ (_) |  _| |_ \\ V  V / (_| | | |  __/");
+        Console.WriteLine("      | |.-\"\"-.|    \\___|_| |_|\\___|_|   \\__, | .__/ \\__|_|\\___/|_| |_|   |___/\\___/|_|  \\__| \\_/\\_/ \\__,_|_|  \\___|");
+        Console.WriteLine("     ///`.::::.`\\                        |___/|_|");
         Console.WriteLine("    ||| ::/  \\:: ;"); 
         Console.WriteLine("    ||; ::\\__/:: ;"); 
         Console.WriteLine("     \\\\\\ '::::' /"); 
         Console.WriteLine("      `=':-..-'`");
-        Console.WriteLine();
+        Console.WriteLine("\n\n");
     }
 
 
